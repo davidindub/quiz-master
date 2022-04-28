f783d0e76c961d645be6fcde5fbcd3c047b11250
David <davidpkelly@me.com>
2022-04-28 22:07:45 +0000

    Remove testing data

--- create_gform_items.py
+++ create_gform_items.py
@@ -1,97 +1,4 @@
 from create_quiz import QUIZ_CATEGORIES
-TEST_QUESTION = {'category': 'General Knowledge', 'type': 'multiple',
-                 'difficulty': 'easy', 'question': 'Which sign of the zodiac comes between Virgo and Scorpio?',
-                 'correct_answer': 'Libra',
-                 'incorrect_answers': ['Gemini', 'Taurus', 'Capricorn']}
-
-TEST_ROUND = {'round_num': 1, 'num_qs': 5, 'category': 9, 'difficulty': 'medium',
-              'question_data':
-              [{'category': 'General Knowledge', 'type': 'multiple', 'difficulty': 'medium',
-                'question': 'Which essential condiment is also known as Japanese horseradish?',
-                'correct_answer': 'Wasabi ',
-                'incorrect_answers': ['Mentsuyu', 'Karashi', 'Ponzu']},
-               {'category': 'General Knowledge', 'type': 'multiple', 'difficulty': 'medium',
-                'question': 'What is the Swedish word for &quot;window&quot;?',
-                  'correct_answer': 'F&ouml;nster',
-                  'incorrect_answers': ['H&aring;l', 'Sk&auml;rm', 'Ruta']},
-                  {'category': 'General Knowledge', 'type': 'multiple', 'difficulty': 'medium',
-                   'question': 'Amsterdam Centraal station is twinned with what station?',
-                   'correct_answer': 'London Liverpool Street',
-                   'incorrect_answers': ['Frankfurt (Main) Hauptbahnhof', 'Paris Gare du Nord', 'Brussels Midi']},
-                  {'category': 'General Knowledge', 'type': 'multiple', 'difficulty': 'medium',
-                   'question': 'Computer manufacturer Compaq was acquired for $25 billion dollars in 2002 by which company?',
-                   'correct_answer': 'Hewlett-Packard',
-                   'incorrect_answers': ['Toshiba', 'Asus', 'Dell']},
-                  {'category': 'General Knowledge', 'type': 'multiple', 'difficulty': 'medium',
-                   'question': 'Which American manufactured submachine gun was informally known by the American soldiers that used it as &quot;Grease Gun&quot;?',
-                   'correct_answer': 'M3', 'incorrect_answers': ['Colt 9mm', 'Thompson', 'MAC-10']}]}
-
-TEST_GAME = {'quiz_title': 'Quick Quiz', 'num_rounds': 2, 'num_questions': 3, 'categories': [9, 9], 'difficulty': 'easy', 'rounds': [{'round_num': 1, 'num_qs': 5, 'category': 9, 'difficulty': 'medium',
-                                                                                                                                      'question_data':
-                                                                                                                                      [{'category': 'General Knowledge', 'type': 'multiple', 'difficulty': 'medium',
-                                                                                                                                        'question': 'ROUND 1 Which essential condiment is also known as Japanese horseradish?',
-                                                                                                                                        'correct_answer': 'Wasabi ',
-                                                                                                                                        'incorrect_answers': ['Mentsuyu', 'Karashi', 'Ponzu']},
-                                                                                                                                          {'category': 'General Knowledge', 'type': 'multiple', 'difficulty': 'medium',
-                                                                                                                                           'question': 'What is the Swedish word for &quot;window&quot;?',
-                                                                                                                                           'correct_answer': 'F&ouml;nster',
-                                                                                                                                           'incorrect_answers': ['H&aring;l', 'Sk&auml;rm', 'Ruta']},
-                                                                                                                                          {'category': 'General Knowledge', 'type': 'multiple', 'difficulty': 'medium',
-                                                                                                                                           'question': 'Amsterdam Centraal station is twinned with what station?',
-                                                                                                                                           'correct_answer': 'London Liverpool Street',
-                                                                                                                                           'incorrect_answers': ['Frankfurt (Main) Hauptbahnhof', 'Paris Gare du Nord', 'Brussels Midi']},
-                                                                                                                                          {'category': 'General Knowledge', 'type': 'multiple', 'difficulty': 'medium',
-                                                                                                                                           'question': 'Computer manufacturer Compaq was acquired for $25 billion dollars in 2002 by which company?',
-                                                                                                                                           'correct_answer': 'Hewlett-Packard',
-                                                                                                                                           'incorrect_answers': ['Toshiba', 'Asus', 'Dell']},
-                                                                                                                                          {'category': 'General Knowledge', 'type': 'multiple', 'difficulty': 'medium',
-                                                                                                                                           'question': 'Which American manufactured submachine gun was informally known by the American soldiers that used it as &quot;Grease Gun&quot;?',
-                                                                                                                                           'correct_answer': 'M3', 'incorrect_answers': ['Colt 9mm', 'Thompson', 'MAC-10']}]},
-
-                                                                                                                                     {'round_num': 2, 'num_qs': 5, 'category': 9, 'difficulty': 'medium',
-                                                                                                                                      'question_data':
-                                                                                                                                      [{'category': 'General Knowledge', 'type': 'multiple', 'difficulty': 'medium',
-                                                                                                                                        'question': 'ROUND 2 Which essential condiment is also known as Japanese horseradish?',
-                                                                                                                                        'correct_answer': 'Wasabi ',
-                                                                                                                                        'incorrect_answers': ['Mentsuyu', 'Karashi', 'Ponzu']},
-                                                                                                                                          {'category': 'General Knowledge', 'type': 'multiple', 'difficulty': 'medium',
-                                                                                                                                           'question': 'What is the Swedish word for &quot;window&quot;?',
-                                                                                                                                           'correct_answer': 'F&ouml;nster',
-                                                                                                                                           'incorrect_answers': ['H&aring;l', 'Sk&auml;rm', 'Ruta']},
-                                                                                                                                          {'category': 'General Knowledge', 'type': 'multiple', 'difficulty': 'medium',
-                                                                                                                                           'question': 'Amsterdam Centraal station is twinned with what station?',
-                                                                                                                                           'correct_answer': 'London Liverpool Street',
-                                                                                                                                           'incorrect_answers': ['Frankfurt (Main) Hauptbahnhof', 'Paris Gare du Nord', 'Brussels Midi']},
-                                                                                                                                          {'category': 'General Knowledge', 'type': 'multiple', 'difficulty': 'medium',
-                                                                                                                                           'question': 'Computer manufacturer Compaq was acquired for $25 billion dollars in 2002 by which company?',
-                                                                                                                                           'correct_answer': 'Hewlett-Packard',
-                                                                                                                                           'incorrect_answers': ['Toshiba', 'Asus', 'Dell']},
-                                                                                                                                          {'category': 'General Knowledge', 'type': 'multiple', 'difficulty': 'medium',
-                                                                                                                                           'question': 'Which American manufactured submachine gun was informally known by the American soldiers that used it as &quot;Grease Gun&quot;?',
-                                                                                                                                           'correct_answer': 'M3', 'incorrect_answers': ['Colt 9mm', 'Thompson', 'MAC-10']}]},
-
-                                                                                                                                     {'round_num': 3, 'num_qs': 5, 'category': 9, 'difficulty': 'medium',
-                                                                                                                                      'question_data':
-                                                                                                                                      [{'category': 'General Knowledge', 'type': 'multiple', 'difficulty': 'medium',
-                                                                                                                                        'question': 'ROUND 3 Which essential condiment is also known as Japanese horseradish?',
-                                                                                                                                        'correct_answer': 'Wasabi ',
-                                                                                                                                        'incorrect_answers': ['Mentsuyu', 'Karashi', 'Ponzu']},
-                                                                                                                                       {'category': 'General Knowledge', 'type': 'multiple', 'difficulty': 'medium',
-                                                                                                                                        'question': 'What is the Swedish word for &quot;window&quot;?',
-                                                                                                                                           'correct_answer': 'F&ouml;nster',
-                                                                                                                                           'incorrect_answers': ['H&aring;l', 'Sk&auml;rm', 'Ruta']},
-                                                                                                                                       {'category': 'General Knowledge', 'type': 'multiple', 'difficulty': 'medium',
-                                                                                                                                        'question': 'Amsterdam Centraal station is twinned with what station?',
-                                                                                                                                           'correct_answer': 'London Liverpool Street',
-                                                                                                                                           'incorrect_answers': ['Frankfurt (Main) Hauptbahnhof', 'Paris Gare du Nord', 'Brussels Midi']},
-                                                                                                                                       {'category': 'General Knowledge', 'type': 'multiple', 'difficulty': 'medium',
-                                                                                                                                        'question': 'Computer manufacturer Compaq was acquired for $25 billion dollars in 2002 by which company?',
-                                                                                                                                           'correct_answer': 'Hewlett-Packard',
-                                                                                                                                           'incorrect_answers': ['Toshiba', 'Asus', 'Dell']},
-                                                                                                                                       {'category': 'General Knowledge', 'type': 'multiple', 'difficulty': 'medium',
-                                                                                                                                        'question': 'Which American manufactured submachine gun was informally known by the American soldiers that used it as &quot;Grease Gun&quot;?',
-                                                                                                                                           'correct_answer': 'M3', 'incorrect_answers': ['Colt 9mm', 'Thompson', 'MAC-10']}]}
-                                                                                                                                     ]}
 
 
 def create_gform_question(question_obj):
@@ -103,7 +10,7 @@ def create_gform_question(question_obj):
         question_obj: A Question object
 
     """
-    
+
     return {
         "createItem": {
             "item": {
